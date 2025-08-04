758db441e1058090f485a9ffab8316fef408dfa6
kalval03 <kalvalakalyani8@gmail.com>
2025-08-04 16:35:36 +0530

    commited

---
+++ javapractice/src/javapractice/Demo.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class Demo{
+public void showGreetings() {
+     System.out.print("Hi All,Welcome to TNS!");
+ }
+ public static void main(String args[]) {
+	   Demo obj = new Demo();
+	   obj.showGreetings(); 
+}
+}
--- javapractice/src/javapractice/Hello.java
+++ javapractice/src/javapractice/Hello.java
@@ -1,10 +1,7 @@
 package javapractice;
 
 public class Hello {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
-	}
-
+	public static void main(String args[]) {
+	System.out.print("Hello");
+}
 }
