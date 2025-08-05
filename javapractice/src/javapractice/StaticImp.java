1eac836f86f29ff2780e2d5bb7852fc855038836
kalval03 <kalvalakalyani8@gmail.com>
2025-08-05 18:23:24 +0530

    static implementation

---
+++ javapractice/src/javapractice/StaticImp.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class StaticImp {
+	String name;
+	int rollno;
+	String branch;
+	static String clgname="Kprit";
+	
+	void display() {
+		System.out.println("Std details: "+name+" "+rollno+" "+branch+" "+clgname);
+	}
+	
+}
