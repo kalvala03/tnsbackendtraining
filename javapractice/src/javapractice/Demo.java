ee171868d8c0b42d2511fd52b9c5698d6d5d7ab1
kalval03 <kalvalakalyani8@gmail.com>
2025-08-05 11:35:47 +0530

    committed successfully

--- javapractice/src/javapractice/Demo.java
+++ javapractice/src/javapractice/Demo.java
@@ -1,11 +1,14 @@
 package javapractice;
 
-class Demo{
-public void showGreetings() {
-     System.out.print("Hi All,Welcome to TNS!");
- }
- public static void main(String args[]) {
-	   Demo obj = new Demo();
-	   obj.showGreetings(); 
+class Sample{
+	public void greetings() {
+		System.out.println("Welcome to TNS!");
+	}
 }
+public class Demo{
+	public static void main(String[]args) {
+		Sample obj=new Sample();
+		obj.greetings();
+	}
+	
 }
