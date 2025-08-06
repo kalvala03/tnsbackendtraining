98d44aad7c8273808a238eccd5160c60908b565c
kalval03 <kalvalakalyani8@gmail.com>
2025-08-06 18:05:29 +0530

    success

---
+++ javapractice/src/javapractice/TypeCasting.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+public class TypeCasting {
+	
+	//Widening Casting (automatically)
+	public static void main(String[] args) {
+		int a=10;
+		float b=a; //converting a smaller type(int) to a larger type size(flaot)
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
