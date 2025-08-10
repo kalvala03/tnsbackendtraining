b5bd4c75020182893415200b88dea26c59498260
kalval03 <kalvalakalyani8@gmail.com>
2025-08-10 20:05:27 +0530

    reverse num

---
+++ javapractice/src/javapractice/WhileLoop.java
@@ -0,0 +1,21 @@
+package javapractice;
+import java.util.Scanner;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// Reversing a number
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter num:");
+		int num=sc.nextInt();
+		int rev=0;
+		while(num>0) {
+			int rem=num%10;
+			rev=rev*10+rem;
+			num/=10;
+		}
+		System.out.println("reverse is:"+rev);
+
+	}
+
+}
