e849805ee9e6d00bb32a18bcab9cfe41d5978637
kalval03 <kalvalakalyani8@gmail.com>
2025-08-17 22:08:51 +0530

    Encapsulation

---
+++ javapractice/src/com/kprit/tns/encapsulation/BankAccMain.java
@@ -0,0 +1,13 @@
+package com.kprit.tns.encapsulation;
+
+public class BankAccMain {
+	public static void main(String[] args) {
+        BankAccount acc = new BankAccount(5000);
+
+        acc.deposit(2000);            // Deposited: 2000
+        acc.withdraw(1500);           // Withdrew: 1500
+        System.out.println("Balance: " + acc.getBalance()); // Balance: 5500
+
+        // acc.balance = 100000;  ❌ ERROR bcz 'balance' is private
+    }
+}
---
+++ javapractice/src/com/kprit/tns/encapsulation/BankAccount.java
@@ -0,0 +1,41 @@
+package com.kprit.tns.encapsulation;
+
+//Encapsulation means binding data and methods together in a single class while hiding the internal details.
+//Here, 'balance' is private 
+//Only controlled access is given through getters and setters (methods).
+
+public class BankAccount {
+	
+	// private variable (data hiding)
+    private double balance;
+
+    // constructor
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    // public method to deposit
+    public void deposit(double depamt) {
+        if(depamt > 0) {
+            balance += depamt;
+            System.out.println("Deposited: " + depamt);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    // public method to withdraw
+    public void withdraw(double withamt) {
+        if(withamt > 0 && withamt <= balance) {
+            balance -= withamt;
+            System.out.println("Withdrew: " + withamt);
+        } else {
+            System.out.println("Insufficient balance or invalid amount!");
+        }
+    }
+
+    // public method to check balance
+    public double getBalance() {
+        return balance;
+    }
+}
