5633f28a4c67d7e717986215e8791ff9754951be
kalval03 <kalvalakalyani8@gmail.com>
2025-08-30 16:08:49 +0530

    success

---
+++ javapractice/src/com/kprit/thiskeyword/ThisInConstructors.java
@@ -0,0 +1,96 @@
+package com.kprit.thiskeyword;
+
+public class ThisInConstructors {
+	
+	//Instance Variables
+	String Author;
+	String Title;
+	int publicationyear;
+	
+	/*
+	//Calling another constructor using 'this'(...)
+	public ThisInConstructors () {
+		 this("Sucheta Raj Khanna", "The Stars Light the Way", 2025); // here  Calls parameterized constructor
+		 System.out.println("This is a Default Constructor");
+	}
+	*/
+	
+	 // Initializes instance variables 
+	public ThisInConstructors(){
+		this.Author="Sucheta Raj Khanna";
+		this.Title="The Stars Light the Way";
+		this.publicationyear=2025;
+		System.out.println("This is a Default Constructor");
+	}
+	
+	// Uses 'this' to differentiate between instance variables and parameters
+	ThisInConstructors(String author,String title,int publicationyear){
+		this.Author=author;  
+		this.Title=title;    	
+		this.publicationyear=publicationyear;
+		System.out.println("This is a Parameterized Constructor");
+	}
+	
+	// Uses 'this' to assign from another object
+	ThisInConstructors(ThisInConstructors dupbook){
+		this.Author=dupbook.Author;
+		this.Title=dupbook.Title;
+		this.publicationyear=dupbook.publicationyear;
+		System.out.println("This is a copy Constructor");
+	}
+	
+	//Method that uses 'this' to refer to the current object
+    void printThisObject() {
+        System.out.println("Current object reference: " + this);
+    }
+    
+    //Method chaining using 'this'
+    ThisInConstructors setAuthor(String Author) {
+        this.Author = Author;
+        return this; // returns the current object
+    }
+    
+    ThisInConstructors setTitle(String Title) {
+        this.Title = Title;
+        return this;
+    }
+    
+    ThisInConstructors setPublicationYear(int year) {
+        this.publicationyear = year;
+        return this;
+    }
+
+    
+public static void main(String[]args) {
+		
+		//uses default constuctor
+	ThisInConstructors obj1=new ThisInConstructors();   
+		System.out.println(obj1.Author);
+		System.out.println(obj1.Title);
+		System.out.println(obj1.publicationyear);
+		
+		// Call the method to print the current object reference
+		obj1.printThisObject();
+		
+		//uses parameterized constructor
+		ThisInConstructors obj2=new ThisInConstructors("AI on Trial","Shri Sujeet Kumar",2025); 
+		System.out.println(obj2.Author);
+		System.out.println(obj2.Title);
+		System.out.println(obj2.publicationyear);
+		
+		//uses copy constructor
+		ThisInConstructors obj3=new ThisInConstructors(obj2);   // copies book2(bc2)
+		System.out.println(obj3.Author);
+		System.out.println(obj3.Title);
+		System.out.println(obj3.publicationyear);
+		
+		// Method chaining example
+        obj3.setAuthor("Kalyani")
+           .setTitle("Learning Java")
+           .setPublicationYear(2026);
+        System.out.println(obj3.Author);
+        System.out.println(obj3.Title);
+        System.out.println(obj3.publicationyear);
+		
+		}
+}
--- javapractice/src/com/kprit/tns/constructor/ConstructorDemo.java
+++ javapractice/src/com/kprit/tns/constructor/ConstructorDemo.java
@@ -1,109 +1,55 @@
 package com.kprit.tns.constructor;
 
 public class ConstructorDemo {
-	String Author;
-	String Title;
-	int publicationyear;
+	String name;
+	int rollno;
 	
 	/*
-	//In Java, if we don’t initialize instance variables, they automatically get default values 
-	// for example, null for objects like String and 0 for numeric types like int.
-	BookConstuctor(){
-		// No initialization here
-	    System.out.println("Default Book created.");
-	}
+	Note-1--->If no constructor is written in a class, Java automatically provides a default constructor (with no parameters).
+	It initializes object variables with default values (like 0, null, false).
 	
-	//Calling another constructor using this(...)
-	BookConstuctor() {
-	        this("Sucheta Raj Khanna", "The Stars Light the Way", 2025); // here  Calls parameterized constructor
-	        System.out.println("This is a Default Constructor");
-	    }
+	
+	Note-2--->In Java, if we don’t initialize instance variables, they automatically get default values 
+	for example, null for objects like String and 0 for numeric types like int.
+	public ConstructorDemo(){
+		// No initialization here
+	}
 	*/
 	
 	
-	// 1. Default Constructor (No-argument constructor)
-    // Automatically provided by Java if no other constructor is defined.
-    // Initializes instance variables with default values.
-	ConstructorDemo(){
-		this.Author="Sucheta Raj Khanna";
-		this.Title="The Stars Light the Way";
-		this.publicationyear=2025;
-		System.out.println("This is a Default Constructor");
+	// 1. Non-parametarized Constructor
+	public ConstructorDemo(){
+		name="kalyani";
+		rollno=548;
+		System.out.println("This is a Non-Parameterized Constructor");
 	}
 	
 	// 2. Parameterized Constructor
-	// Uses this to differentiate between instance variables and parameters
-	ConstructorDemo(String author,String title,int publicationyear){
-		
-		this.Author=author;  
-		this.Title=title;    	
-		this.publicationyear=publicationyear;
+	public ConstructorDemo(String name,int rollno){
+		this.name=name;
+		this.rollno=rollno;
 		System.out.println("This is a Parameterized Constructor");
 	}
 	
 	// 3. Copy Constructor
     // Creates a new object by copying the values of an existing object.
-	// Uses this to assign from another object
 	ConstructorDemo(ConstructorDemo dupbook){
-		this.Author=dupbook.Author;
-		this.Title=dupbook.Title;
-		this.publicationyear=dupbook.publicationyear;
+		this.name=dupbook.name;
+		this.rollno=dupbook.rollno;
 		System.out.println("This is a copy Constructor");
 	}
 	
-	 //  Method that uses this to refer to the current object
-    void printThisObject() {
-        System.out.println("Current object reference: " + this);
-    }
-	
-    //  Method chaining using this
-    ConstructorDemo setAuthor(String Author) {
-        this.Author = Author;
-        return this; // returns the current object
-    }
-    
-    ConstructorDemo setTitle(String Title) {
-        this.Title = Title;
-        return this;
-    }
-    
-    ConstructorDemo setPublicationYear(int year) {
-        this.publicationyear = year;
-        return this;
-    }
-
-
 
 	public static void main(String[]args) {
+		ConstructorDemo obj=new ConstructorDemo();   //calls Non-parametarized Constructor
+		System.out.println(obj.name);
+		System.out.println(obj.rollno);
+		ConstructorDemo obj1=new ConstructorDemo("Kavitha", 123);  //calls parametarized Constructor
+		System.out.println(obj1.name);
+		System.out.println(obj1.rollno);
+		ConstructorDemo obj2=new ConstructorDemo(obj1);  //calls copy Constructor
+		System.out.println(obj2.name);
+		System.out.println(obj2.rollno);
 		
-		//uses default constuctor
-		ConstructorDemo bc1=new ConstructorDemo();   
-		System.out.println(bc1.Author);
-		System.out.println(bc1.Title);
-		System.out.println(bc1.publicationyear);
-		
-		// Call the method to print the current object reference
-		bc1.printThisObject();
-		
-		//uses parameterized constructor
-		ConstructorDemo bc2=new ConstructorDemo("AI on Trial","Shri Sujeet Kumar",2025); 
-		System.out.println(bc2.Author);
-		System.out.println(bc2.Title);
-		System.out.println(bc2.publicationyear);
-		
-		//uses copy constructor
-		ConstructorDemo bc3=new ConstructorDemo(bc2);   // copies book2(bc2)
-		System.out.println(bc3.Author);
-		System.out.println(bc3.Title);
-		System.out.println(bc3.publicationyear);
-		
-		// Method chaining example
-        bc1.setAuthor("Kalyani")
-           .setTitle("Learning Java")
-           .setPublicationYear(2026);
-        System.out.println(bc1.Author);
-        System.out.println(bc1.Title);
-        System.out.println(bc1.publicationyear);
-		
-		}
+	}
 }
