4cf89bfa4618219c5e147b7116f73cf241394063
kalval03 <kalvalakalyani8@gmail.com>
2025-08-30 19:25:57 +0530

    success

--- javapractice/src/com/kprit/multithreading/AnonymousClassDemo.java
+++ javapractice/src/com/kprit/multithreading/AnonymousClassDemo.java
@@ -1,23 +1,21 @@
 package com.kprit.multithreading;
 
 public class AnonymousClassDemo {
-
-    // Runnable object created using anonymous class
-    Runnable r1 = new Runnable() {
-        @Override
-        public void run() {
-            System.out.println("Task executed by: " + Thread.currentThread().getName());
-        }
-    };
-
     public static void main(String[] args) {
-        // Create outer class object to access non-static Runnable
-        AnonymousClassDemo obj = new AnonymousClassDemo();
+        
+        // 1. Creating a thread by extending Thread using anonymous class
+        new Thread() {
+            public void run() {
+                System.out.println("Extending thread: "+Thread.currentThread().getName());
+            }
+        }.start();
+        
 
-        // Creating a thread using the anonymous Runnable object
-        Thread t1 = new Thread(obj.r1);
-
-        // Start the thread
-        t1.start();
+        // 2. Creating a thread by implementing Runnable using anonymous class
+        new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Implementing Runnable: "+Thread.currentThread().getName());
+            }
+        }).start();
     }
 }
--- javapractice/src/com/kprit/multithreading/LambdaThreadDemo.java
+++ javapractice/src/com/kprit/multithreading/LambdaThreadDemo.java
@@ -1,21 +1,23 @@
 package com.kprit.multithreading;
 
+//  Lambda simplifies the syntax instead of writing an anonymous class.
+//Only functional interfaces can use Lambda expressions.So only Runnable can use Lambda
+//but Thread itself is a class, not a functional interface, so you cannot directly extend Thread using a Lambda.
 public class LambdaThreadDemo {
 
-	 public static void main(String[] args) {
-	        // Runnable using Lambda
-	        Runnable r1= () -> {
-	            for (int i = 1; i <= 5; i++) {
-	                System.out.println(Thread.currentThread().getName() + " -> " + i);
-	            }
-	        };
+    public static void main(String[] args) {
+        
+        // Creating a Runnable using a Lambda expression
+        Runnable r1 = () -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " -> " + i);
+            }
+        };
 
-	        // Create threads with the same task
-	        Thread t1 = new Thread(r1, "Lambda-1");
-	        Thread t2 = new Thread(r1, "Lambda-2");
-
-	        // Start threads
-	        t1.start();
-	        t2.start();
-	    }
+        Thread t1 = new Thread(r1, "Lambda-1");  
+        Thread t2 = new Thread(r1, "Lambda-2");  
+        
+        t1.start();
+        t2.start();
+    }
 }
