da945c06a07934cfc5428a8ebc51d954ddbce06f
kalval03 <kalvalakalyani8@gmail.com>
2025-08-31 17:30:44 +0530

    success

--- javapractice/src/com/kprit/multithreading/DefaultThread.java
+++ javapractice/src/com/kprit/multithreading/DefaultThread.java
@@ -3,21 +3,21 @@ package com.kprit.multithreading;
 public class DefaultThread {
 
     public static void main(String[] args) {
-        // Default (main) thread details 
-        System.out.println("This is Default Thread");
+
+    	//Default Thread
         System.out.println("Active Thread Count: " + Thread.activeCount()); 
-        System.out.println("Thread Name: " + Thread.currentThread().getName()); 
-        System.out.println("Thread Priority: " + Thread.currentThread().getPriority()); // default is 5
+        System.out.println("Default Thread Name: " + Thread.currentThread().getName()); 
+        System.out.println("Default Thread Priority: " + Thread.currentThread().getPriority()); // default is 5
 
         // User-defined thread creation 
         Thread t1 = new Thread(); // creates a new thread (but no custom run() logic)
-        System.out.println(t1.getState());
+        System.out.println("State when Thread object is created but not started: "+t1.getState());
         
         t1.start(); // starts the new thread (does nothing since run() is empty)
-        System.out.println(t1.getState());
+        System.out.println("State when started and is ready to run: "+t1.getState());
         
         System.out.println("Active Thread Count: " + Thread.activeCount());
-        System.out.println(t1.getState());
+        System.out.println("State when Thread has finished execution: "+t1.getState());
         
         System.out.println("Active Thread Count: " + Thread.activeCount());
     }
